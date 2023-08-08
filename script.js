e9c5bf15e6b566da499f09f4cc90c97a87fed8f2
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-07 18:50:35 -0700

    Calculator interpreter completed!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const numToCalc = [];
+
 function initialize() {
     const buttonRowArray = (document.querySelectorAll('.buttonRow > button'));
     
@@ -16,7 +18,37 @@ function buttonInterpreter(buttonPressed) {
     {
         console.log(parseInt(buttonPressed.textContent));
         display.textContent += parseInt(buttonPressed.textContent);
-
+    }
+    else if (buttonPressed.textContent === '.')
+    {
+        if (!(display.textContent).includes("."))
+        {
+            if (display.textContent.length === 0)
+            {
+                display.textContent += 0;
+            }
+            display.textContent += '.';
+        }
+    }
+    else if (buttonPressed.textContent === '=')
+    {
+        if (display.textContent != "")
+        {
+            numToCalc.push(parseFloat(display.textContent));
+            display.textContent = "";
+            console.table(numToCalc);
+            numToCalc.splice(0, numToCalc.length);
+        }
+    }
+    else
+    {
+        if (display.textContent != "")
+        {
+            numToCalc.push(parseFloat(display.textContent));
+            numToCalc.push(buttonPressed.textContent);
+            display.textContent = "";
+            console.table(numToCalc);
+        }
     }
 }
 
